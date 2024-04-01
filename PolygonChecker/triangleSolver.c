846adbeb9fbb373280dcf398c2a6b770f768f0f8
Mohammed Sujahat Ali <mali1237@conestogac.on.ca>
2024-04-01 14:39:10 -0400

    final model with all the chnages

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -23,6 +23,13 @@ TriangleType classifyTriangle(int side1, int side2, int side3) {
     }
 }
 
+// Function to calculate the internal angles of a triangle
+void calculateTriangleAngles(int side1, int side2, int side3, double* angle1, double* angle2, double* angle3) {
+    *angle1 = acos((pow(side2, 2) + pow(side3, 2) - pow(side1, 2)) / (2.0 * side2 * side3));
+    *angle2 = acos((pow(side1, 2) + pow(side3, 2) - pow(side2, 2)) / (2.0 * side1 * side3));
+    *angle3 = acos((pow(side1, 2) + pow(side2, 2) - pow(side3, 2)) / (2.0 * side1 * side2));
+}
+
 // Function to check if one triangle is inside another triangle
 bool isTriangleInsideTriangle(int x1, int y1, int x2, int y2, int x3, int y3,
     int x4, int y4, int x5, int y5, int x6, int y6) {
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -13,6 +13,9 @@ typedef enum {
 // Function to classify a triangle based on its side lengths
 TriangleType classifyTriangle(int side1, int side2, int side3);
 
+// Function to calculate the internal angles of a triangle
+void calculateTriangleAngles(int side1, int side2, int side3, double* angle1, double* angle2, double* angle3);
+
 // Function to check if one triangle is inside another triangle
 bool isTriangleInsideTriangle(int x1, int y1, int x2, int y2, int x3, int y3,
     int x4, int y4, int x5, int y5, int x6, int y6);
